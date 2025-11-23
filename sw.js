08b130a3b5766e666d03e1cedb88bc6a8b41d742
Trandan1092 <105041628+Trandan1092@users.noreply.github.com>
2025-11-23 18:34:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'block-blast-v1';
+const CACHE_NAME = 'block-blast-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,13 +9,15 @@ const urlsToCache = [
   './sounds/clear.mp3',
   './sounds/gameover.mp3',
   './icon-192x192.png',
-  './icon-512x512.png'
+  './icon-512x512.png',
+  './manifest.json'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -25,11 +27,58 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
+        // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        ).catch(function() {
+          // If both cache and network fail, show offline page
+          return caches.match('./index.html');
+        });
       }
     )
   );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Listen for messages from the client
+self.addEventListener('message', function(event) {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
